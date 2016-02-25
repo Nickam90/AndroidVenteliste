e7927350e9f8ae720aa0ee0d34c168bb07d98d20
Nicolai Kamstrup Kristiansen <Nicolai Kamstrup Kristiansen>
2016-02-25 21:18:12 +0100

    First commit

---
+++ app/src/main/java/com/f2016/dtu/androidventeliste/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package com.f2016.dtu.androidventeliste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
