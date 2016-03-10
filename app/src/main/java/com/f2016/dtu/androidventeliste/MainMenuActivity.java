817d1616fe5e6f556bef0bf1b9b9e13e6662f007
Nickam90 <Nickam90@hotmail.com>
2016-03-10 18:53:54 +0100

    changed layout

--- app/src/main/java/com/f2016/dtu/androidventeliste/MainMenuActivity.java
+++ app/src/main/java/com/f2016/dtu/androidventeliste/MainMenuActivity.java
@@ -2,12 +2,46 @@ package com.f2016.dtu.androidventeliste;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    EditText loginNumberInput1;
+    EditText loginNumberInput2;
+    EditText loginNumberInput3;
+    EditText loginNumberInput4;
+    EditText loginNumberInput5;
+    EditText loginNumberInput6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginNumberInput1 = (EditText)findViewById(R.id.loginNumberField1);
+        loginNumberInput2 = (EditText)findViewById(R.id.loginNumberField2);
+        loginNumberInput3 = (EditText)findViewById(R.id.loginNumberField3);
+        loginNumberInput4 = (EditText)findViewById(R.id.loginNumberField4);
+        loginNumberInput5 = (EditText)findViewById(R.id.loginNumberField5);
+        loginNumberInput6 = (EditText)findViewById(R.id.loginNumberField6);
+
+        loginNumberInput1.setOnKeyListener(keyPressHandler);
+
+        loginNumberInput1.requestFocus();
+
     }
+
+    View.OnKeyListener keyPressHandler = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if(event.getAction() == KeyEvent.ACTION_UP ){
+                Log.d("KeyEvent UP", "Pressed");
+
+            }
+            return false;
+        }
+    };
+
 }
