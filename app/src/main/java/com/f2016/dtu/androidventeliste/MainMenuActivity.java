2e32c294f6a46c0511a8cfba34b063ef167464aa
Nickam90 <Nickam90@hotmail.com>
2016-03-12 22:05:30 +0100

    changesbkhbjhdhd

--- app/src/main/java/com/f2016/dtu/androidventeliste/MainMenuActivity.java
+++ app/src/main/java/com/f2016/dtu/androidventeliste/MainMenuActivity.java
@@ -15,6 +15,7 @@ public class MainMenuActivity extends AppCompatActivity {
     private EditText loginNumberInput4;
     private EditText loginNumberInput5;
     private EditText loginNumberInput6;
+    int focusIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,19 +35,124 @@ public class MainMenuActivity extends AppCompatActivity {
         loginNumberInput5.setOnKeyListener(keyPressHandler);
         loginNumberInput6.setOnKeyListener(keyPressHandler);
 
+        loginNumberInput1.setOnFocusChangeListener(focusChangeHandler);
+        loginNumberInput2.setOnFocusChangeListener(focusChangeHandler);
+        loginNumberInput3.setOnFocusChangeListener(focusChangeHandler);
+        loginNumberInput4.setOnFocusChangeListener(focusChangeHandler);
+        loginNumberInput5.setOnFocusChangeListener(focusChangeHandler);
+        loginNumberInput6.setOnFocusChangeListener(focusChangeHandler);
+
         loginNumberInput1.requestFocus();
 
     }
 
+    View.OnFocusChangeListener focusChangeHandler = new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            //Log.d("KeyEvent UP", "Pressed " + v.getContext().getResources() + ", " + hasFocus);
+
+            if(v == loginNumberInput1 && hasFocus){
+                loginNumberInput1.setText("");
+                focusIndex = 0;
+            }
+            else if(v == loginNumberInput2 && hasFocus){
+                loginNumberInput2.setText("");
+                focusIndex = 1;
+            }
+            else if(v == loginNumberInput3 && hasFocus){
+                loginNumberInput3.setText("");
+                focusIndex = 2;
+            }
+            else if(v == loginNumberInput4 && hasFocus){
+                loginNumberInput4.setText("");
+                focusIndex = 3;
+            }
+            else if(v == loginNumberInput5 && hasFocus){
+                loginNumberInput5.setText("");
+                focusIndex = 4;
+            }
+            else if(v == loginNumberInput6 && hasFocus){
+                loginNumberInput6.setText("");
+                focusIndex = 5;
+            }
+        }
+    };
+
     View.OnKeyListener keyPressHandler = new View.OnKeyListener() {
         @Override
         public boolean onKey(View v, int keyCode, KeyEvent event) {
-            if(event.getAction() == KeyEvent.ACTION_UP ){
-                Log.d("KeyEvent UP", "Pressed " + event.getCharacters() + ", " + event.getKeyCode());
-
+            if(event.getAction() == KeyEvent.ACTION_UP ) {
+               // Log.d("KeyEvent UP", "Pressed " + event.getCharacters() + ", " + event.getKeyCode());
+                if (event.getKeyCode() > 6 && event.getKeyCode() < 17) {
+                    changeInputFocus(true);
+                }
+                return true;
             }
             return false;
         }
     };
 
+    private void changeInputFocus(boolean right){
+        int key = focusIndex;
+        if(key < 5 && right){
+            key++;
+        }
+        else if(key > 0 && !right){
+            key--;
+        }
+        else{
+            key = 0;
+            login();
+
+        }
+        switch (key){
+            case 0:
+                loginNumberInput1.requestFocus();
+                focusIndex = 0;
+                break;
+            case 1:
+                loginNumberInput2.requestFocus();
+                focusIndex = 1;
+                break;
+            case 2:
+                loginNumberInput3.requestFocus();
+                focusIndex = 2;
+                break;
+            case 3:
+                loginNumberInput4.requestFocus();
+                focusIndex = 3;
+                break;
+            case 4:
+                loginNumberInput5.requestFocus();
+                focusIndex = 4;
+                break;
+            case 5:
+                loginNumberInput6.requestFocus();
+                focusIndex = 5;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void login() {
+Log.d("Login", "Login");
+        String digit1 = loginNumberInput1.getText().toString();
+        String digit2 = loginNumberInput2.getText().toString();
+        String digit3 = loginNumberInput3.getText().toString();
+        String digit4 = loginNumberInput4.getText().toString();
+        String digit5 = loginNumberInput5.getText().toString();
+        String digit6 = loginNumberInput6.getText().toString();
+
+        String code = digit1+digit2+digit3+digit4+digit5+digit6;
+        Log.d("Login", "Code: " + code);
+
+        loginNumberInput1.setText("");
+        loginNumberInput2.setText("");
+        loginNumberInput3.setText("");
+        loginNumberInput4.setText("");
+        loginNumberInput5.setText("");
+        loginNumberInput6.setText("");
+
+    }
 }
