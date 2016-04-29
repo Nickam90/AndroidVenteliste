9ace16b7a6217036feaf9743bdb1e5460541c87e
BigMac <isoshape@gmail.com>
2016-04-29 11:19:02 +0200

    14 commit

---
+++ app/src/main/java/com/f2016/dtu/androidventeliste/Fragments/HelpFragment.java
@@ -0,0 +1,30 @@
+package com.f2016.dtu.androidventeliste.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.f2016.dtu.androidventeliste.R;
+
+/**
+ * Created by BigMac on 29/04/16.
+ */
+public class HelpFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.trianger, container, false);
+        WebView myWebView = (WebView) view.findViewById(R.id.webView1);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl("file:///android_asset/help.html");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        return view;
+    }
+}
